975cc23e747e54153f02c48938b9c4473087e753
Ricardo Dias Cavalcante <ricardodc@gmail.com>
2019-02-25 19:28:21 -0300

    Query site information

--- README.md
+++ README.md
@@ -95,4 +95,4 @@ Looking for more guidance? Full documentation for Gatsby lives [on the website](
 [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-hello-world)
 
 <!-- AUTO-GENERATED-CONTENT:END -->
-https://medium.com/@thetrevorharmon/how-to-make-a-super-fast-static-site-with-gatsby-typescript-and-sass-3742c00d4524
+  https://medium.com/@thetrevorharmon/how-to-make-a-super-fast-static-site-with-gatsby-typescript-and-sass-3742c00d4524
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
 import * as React from "react";
 import * as styles from './index.module.scss';
+import { graphql } from "gatsby";
 
+interface IndexPageProps {
+    data : {
+        site: {
+            siteMetadata: {
+                name: string;
+                tagline: string;
+            }
+        }
+    }
+};
 
-interface IndexPageProps {};
+export const indexPageQuery = graphql`
+    query indexPageQuery {
+        site {
+            siteMetadata {
+                name
+                tagline
+            }
+        }
+    }
+`;
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
-
     public render() {
+        const { name, tagline } = this.props.data.site.siteMetadata;
         return (
             <div className={styles.container}>
-                <h1>Hello world!</h1>
-                <p>CSS processors are great!</p>
+                <h1>{ name }</h1>
+                <p>{ tagline }</p>
             </div>
         )
     }
